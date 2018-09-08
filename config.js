4399d50476edfa2a1cb15073bb064ec27d1c7e6d
zdy <boxuerixin@hotmail.com>
2018-09-08 09:51:54 +0800

    添加服务器配置

---
+++ config.js
@@ -0,0 +1,5 @@
+const server = "http://192.168.137.1:80/";
+
+module.exports = {
+  server,
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -54,166 +54,222 @@ Page({
   }
 })
 */
-
+const config = require('../../config.js')
 let col1H = 0;
 let col2H = 0;
 wx.setNavigationBarTitle({
-    title: '京小蜜旅行'
+  title: '京小蜜旅行'
 })
 Page({
 
-    data: {
-        scrollH: 0,
-        imgWidth: 0,
-        loadingCount: 0,
-        images: [],
-        col1: [],
-        col2: [],
-        swiperImgs: [
-            'http://localhost:3000/images/1.jpg',
-            'http://localhost:3000/images/2.jpg',
-            'http://localhost:3000/images/3.jpg'
-        ],
-    },
+  data: {
+    scrollH: 0,
+    imgWidth: 0,
+    loadingCount: 0,
+    images: [],
+    col1: [],
+    col2: [],
+    swiperImgs: [
+      config.server + 'images/1.jpg',
+      config.server + 'images/2.jpg',
+      config.server + 'images/3.jpg'
+    ],
+  },
 
-    onLoad: function () {
-        wx.getSystemInfo({
-                success: (res) => {
-                let ww = res.windowWidth;
+  onLoad: function() {
+    wx.getSystemInfo({
+      success: (res) => {
+        let ww = res.windowWidth;
         let wh = res.windowHeight;
         let imgWidth = ww * 0.48;
         let scrollH = wh;
 
         this.setData({
-            scrollH: scrollH,
-            imgWidth: imgWidth
+          scrollH: scrollH,
+          imgWidth: imgWidth
         });
 
         this.loadImages();
-    }
+      }
     })
-    },
-    clickMe:function () {
-        /*wx.getLocation({
-                type: 'wgs84',
-                success: (res) => {
-                var latitude = res.latitude // 经度
-                var longitude = res.longitude // 纬度
-                this.setData({
-                latitude,
-                longitude
-                })
-            }
-        })*/
-        wx.getLocation({
-            type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-            success: function(res) {
-                var latitude = res.latitude
-                var longitude = res.longitude
-                wx.openLocation({
-                    latitude: latitude,
-                    longitude: longitude,
-                    scale: 28
-                })
-            }
+  },
+  clickMe: function() {
+    /*wx.getLocation({
+            type: 'wgs84',
+            success: (res) => {
+            var latitude = res.latitude // 经度
+            var longitude = res.longitude // 纬度
+            this.setData({
+            latitude,
+            longitude
+            })
+        }
+    })*/
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function(res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
         })
-    },
-    video:function(){
-        /*wx.previewImage({
-            current: 'http://localhost:3000/images/1.jpg', // 当前显示图片的http链接
-            urls: ['http://localhost:3000/images/1.jpg','http://localhost:3000/images/1.jpg'] // 需要预览的图片http链接列表
-        })*/
-        wx.showLoading({
-            title:'ddd'
-        })
-    },
-    sCan:function () {
-        wx.scanCode({
-            success:(res)=>{
-                console.log(res)
-            }
-        })
-    },
-    onImageLoad: function (e) {
-        let imageId = e.currentTarget.id;
-        let oImgW = e.detail.width;         //图片原始宽度
-        let oImgH = e.detail.height;        //图片原始高度
-        let imgWidth = this.data.imgWidth;  //图片设置的宽度
-        let scale = imgWidth / oImgW;        //比例计算
-        let imgHeight = oImgH * scale;      //自适应高度
+      }
+    })
+  },
+  video: function() {
+    /*wx.previewImage({
+        current: 'http://localhost:3000/images/1.jpg', // 当前显示图片的http链接
+        urls: ['http://localhost:3000/images/1.jpg','http://localhost:3000/images/1.jpg'] // 需要预览的图片http链接列表
+    })*/
+    wx.showLoading({
+      title: 'ddd'
+    })
+  },
+  sCan: function() {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+  onImageLoad: function(e) {
+    let imageId = e.currentTarget.id;
+    let oImgW = e.detail.width; //图片原始宽度
+    let oImgH = e.detail.height; //图片原始高度
+    let imgWidth = this.data.imgWidth; //图片设置的宽度
+    let scale = imgWidth / oImgW; //比例计算
+    let imgHeight = oImgH * scale; //自适应高度
 
-        let images = this.data.images;
-        let imageObj = null;
+    let images = this.data.images;
+    let imageObj = null;
 
-        for (let i = 0; i < images.length; i++) {
-            let img = images[i];
-            if (img.id === imageId) {
-                imageObj = img;
-                break;
-            }
-        }
-
-        imageObj.height = imgHeight;
-
-        let loadingCount = this.data.loadingCount - 1;
-        let col1 = this.data.col1;
-        let col2 = this.data.col2;
-
-        if (col1H <= col2H) {
-            col1H += imgHeight;
-            col1.push(imageObj);
-        } else {
-            col2H += imgHeight;
-            col2.push(imageObj);
-        }
-
-        let data = {
-            loadingCount: loadingCount,
-            col1: col1,
-            col2: col2
-        };
-
-        if (!loadingCount) {
-            data.images = [];
-        }
-
-        this.setData(data);
-    },
-
-    loadImages: function () {
-        let images = [
-            { pic: "images/1.jpg", height: 0 },
-            { pic: "images/2.jpg", height: 0 },
-            { pic: "images/3.jpg", height: 0 },
-            { pic: "images/4.jpg", height: 0 },
-            { pic: "images/5.jpg", height: 0 },
-            { pic: "images/6.jpg", height: 0 },
-            { pic: "images/7.jpg", height: 0 },
-            { pic: "images/8.jpg", height: 0 },
-            { pic: "images/9.jpg", height: 0 },
-            { pic: "images/10.jpg", height: 0 },
-            { pic: "images/11.jpg", height: 0 },
-            { pic: "images/12.jpg", height: 0 },
-            { pic: "images/13.jpg", height: 0 },
-            { pic: "images/14.jpg", height: 0 },
-            { pic: "images/15.jpg", height: 0 },
-            { pic: "images/16.jpg", height: 0 },
-            { pic: "images/17.jpg", height: 0 },
-            { pic: "images/18.jpg", height: 0 },
-            { pic: "images/19.jpg", height: 0 }
-        ];
-
-        let baseId = "img-" + (+new Date());
-
-        for (let i = 0; i < images.length; i++) {
-            images[i].id = baseId + "-" + i;
-            images[i].pic='http://localhost:3000/'+images[i].pic
-        }
-
-        this.setData({
-            loadingCount: images.length,
-            images: images
-        });
+    for (let i = 0; i < images.length; i++) {
+      let img = images[i];
+      if (img.id === imageId) {
+        imageObj = img;
+        break;
+      }
     }
+
+    imageObj.height = imgHeight;
+
+    let loadingCount = this.data.loadingCount - 1;
+    let col1 = this.data.col1;
+    let col2 = this.data.col2;
+
+    if (col1H <= col2H) {
+      col1H += imgHeight;
+      col1.push(imageObj);
+    } else {
+      col2H += imgHeight;
+      col2.push(imageObj);
+    }
+
+    let data = {
+      loadingCount: loadingCount,
+      col1: col1,
+      col2: col2
+    };
+
+    if (!loadingCount) {
+      data.images = [];
+    }
+
+    this.setData(data);
+  },
+
+  loadImages: function() {
+    let images = [{
+        pic: "images/1.jpg",
+        height: 0
+      },
+      {
+        pic: "images/2.jpg",
+        height: 0
+      },
+      {
+        pic: "images/3.jpg",
+        height: 0
+      },
+      {
+        pic: "images/4.jpg",
+        height: 0
+      },
+      {
+        pic: "images/5.jpg",
+        height: 0
+      },
+      {
+        pic: "images/6.jpg",
+        height: 0
+      },
+      {
+        pic: "images/7.jpg",
+        height: 0
+      },
+      {
+        pic: "images/8.jpg",
+        height: 0
+      },
+      {
+        pic: "images/9.jpg",
+        height: 0
+      },
+      {
+        pic: "images/10.jpg",
+        height: 0
+      },
+      {
+        pic: "images/11.jpg",
+        height: 0
+      },
+      {
+        pic: "images/12.jpg",
+        height: 0
+      },
+      {
+        pic: "images/13.jpg",
+        height: 0
+      },
+      {
+        pic: "images/14.jpg",
+        height: 0
+      },
+      {
+        pic: "images/15.jpg",
+        height: 0
+      },
+      {
+        pic: "images/16.jpg",
+        height: 0
+      },
+      {
+        pic: "images/17.jpg",
+        height: 0
+      },
+      {
+        pic: "images/18.jpg",
+        height: 0
+      },
+      {
+        pic: "images/19.jpg",
+        height: 0
+      }
+    ];
+
+    let baseId = "img-" + (+new Date());
+
+    for (let i = 0; i < images.length; i++) {
+      images[i].id = baseId + "-" + i;
+      images[i].pic = config.server + images[i].pic
+    }
+
+    this.setData({
+      loadingCount: images.length,
+      images: images
+    });
+  }
 
 })
